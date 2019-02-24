72994aea31dac22b9d27b444eb799d862acd904c
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-24 23:17:03 +0100

    fixed untracked files

--- App.js
+++ App.js
@@ -1,37 +1,37 @@
 var contacts = [
     {
-      id: 1,
-      firstName: 'Jan',
-      lastName: 'Nowak',
-      email: 'jan.nowak@example.com',
+        id: 1,
+        firstName: 'Jan',
+        lastName: 'Nowak',
+        email: 'jan.nowak@example.com',
     },
     {
-      id: 2,
-      firstName: 'Adam',
-      lastName: 'Kowalski',
-      email: 'adam.kowalski@example.com',
+        id: 2,
+        firstName: 'Adam',
+        lastName: 'Kowalski',
+        email: 'adam.kowalski@example.com',
     },
     {
-      id: 3,
-      firstName: 'Zbigniew',
-      lastName: 'Koziol',
-      email: 'zbigniew.koziol@example.com',
+        id: 3,
+        firstName: 'Zbigniew',
+        lastName: 'Koziol',
+        email: 'zbigniew.koziol@example.com',
     }
-  ];
-  
-  var contactForm = {
+];
+
+var contactForm = {
     firstName: '',
     lastName: '',
     email: ''
-  };
-  
-  var App = React.createClass({
+};
+
+var App = React.createClass({
     render: function () {
-      return (
-        React.createElement('div', { className: 'app' },
-          React.createElement(ContactForm, { contact: contactForm }),
-          React.createElement(Contacts, { items: contacts }, {})
-        )
-      );
+        return (
+            React.createElement('div', { className: 'app' },
+                React.createElement(ContactForm, { contact: contactForm }),
+                React.createElement(Contacts, { items: contacts }, {})
+            )
+        );
     }
-  });
+});
